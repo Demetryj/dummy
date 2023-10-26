6d20a3d5df9101a19714e011732379aedf87912e
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-10-26 05:54:51 +0300

    Create List.js

---
+++ src/Tabs/List.js
@@ -0,0 +1,17 @@
+import './Tabs.css';
+
+const List = ({ title }) => (
+  <>
+    <h1 className="title">{title}</h1>
+    <table>
+      <tbody>
+        <tr>
+          <td>Dummy</td>
+          <td>List</td>
+        </tr>
+      </tbody>
+    </table>
+  </>
+);
+
+export default List;
