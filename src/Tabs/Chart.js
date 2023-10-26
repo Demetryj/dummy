0ed77e29a6bb4f60f6a453c826d13c1ded32b84b
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-10-26 05:54:49 +0300

    Create Chart.js

---
+++ src/Tabs/Chart.js
@@ -0,0 +1,17 @@
+import './Tabs.css';
+
+const Chart = ({ title }) => (
+  <>
+    <h1 className="title">{title}</h1>
+    <table>
+      <tbody>
+        <tr>
+          <td>Dummy</td>
+          <td>Chart</td>
+        </tr>
+      </tbody>
+    </table>
+  </>
+);
+
+export default Chart;
