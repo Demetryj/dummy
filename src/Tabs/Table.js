479e489e0fc472a22c52a1e6949bfd0957ac0b98
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-10-26 05:54:53 +0300

    Create Table.js

---
+++ src/Tabs/Table.js
@@ -0,0 +1,17 @@
+import './Tabs.css';
+
+const Table = ({ title }) => (
+  <>
+    <h1 className="title">{title}</h1>
+    <table>
+      <tbody>
+        <tr>
+          <td>Dummy</td>
+          <td>Table</td>
+        </tr>
+      </tbody>
+    </table>
+  </>
+);
+
+export default Table;
